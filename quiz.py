8931ae33501ff30c2093adf48e293f004d34a24d
Diana1505rojas <diana.rojas2@udea.edu.co>
2024-04-12 10:56:45 -0500

    punto 3 Se muestran las atributos de la matriz 3d

--- quiz.py
+++ quiz.py
@@ -9,7 +9,7 @@ dim2=20
 dim3=30
 dim4=200
 
-matriz4d= np.random.rand(size=(dim1,dim2,dim3,dim4))
+matriz4d= np.random.randint(10, size=(dim1,dim2,dim3,dim4))
 
 print("PUNTO 1:")
 print(matriz4d.shape)
@@ -21,5 +21,14 @@ matriz3d.shape=(dim1, dim2, -1)
 print("PUNTO 2:")
 print(matriz3d.shape)
 print(matriz3d)
+
+#punto 3
+print(" PUNTO 3 \n")
+print(f"Numero de dimensiones: {matriz3d.ndim}")
+print(f"Forma: {matriz3d.shape}")
+print(f"Tamaño: {matriz3d.size}")
+print(f"Tipo: {matriz3d.dtype}")
+print(f"Tamaño de bytes: {matriz3d.itemsize}")
+print(f"Tamaño total: {matriz3d.nbytes}")
 
 
